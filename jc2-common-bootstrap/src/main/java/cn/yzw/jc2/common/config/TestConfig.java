3e7181c7120bcf898df8d9a547b49a7287a469a7
liangbaole <liangbaole@yzw.cn>
2023-05-29 17:49:02 +0800

    测试

--- jc2-common-bootstrap/src/main/java/cn/yzw/jc2/common/config/TestConfig.java
+++ jc2-common-bootstrap/src/main/java/cn/yzw/jc2/common/config/TestConfig.java
@@ -6,6 +6,7 @@ import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
