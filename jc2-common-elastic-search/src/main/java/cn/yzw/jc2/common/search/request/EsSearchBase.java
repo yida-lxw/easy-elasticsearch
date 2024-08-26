2527320807a6ef3f994ba78b57b32cfb0913245d
liangbaole <liangbaole@yzw.cn>
2024-08-26 17:03:46 +0800

    动态字段检索

--- jc2-common-elastic-search/src/main/java/cn/yzw/jc2/common/search/request/EsSearchBase.java
+++ jc2-common-elastic-search/src/main/java/cn/yzw/jc2/common/search/request/EsSearchBase.java
@@ -1,12 +1,14 @@
 package cn.yzw.jc2.common.search.request;
 
 import lombok.Data;
+import lombok.experimental.SuperBuilder;
 
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
 @Data
+@SuperBuilder
 public class EsSearchBase implements Serializable {
     /**
      * 动态字段；例如搜索字段：onlineDate的时间范围 key=onlineDate,value={"searchType":"esRange","start":20240701,"end":20240730,"includeUpper":true,"includeLower":true}
