191083054be70dd8181a1b6bac38d047a1d2211f
yangle <yangle@yzw.cn>
2024-10-29 18:10:32 +0800

    分库分表全量同步

--- jc2-common-data-transfer/src/main/java/cn/yzw/jc2/common/transfer/common/AbstractDataTransferBase.java
+++ jc2-common-data-transfer/src/main/java/cn/yzw/jc2/common/transfer/common/AbstractDataTransferBase.java
@@ -30,10 +30,12 @@ public abstract class AbstractDataTransferBase {
             String[] beanNames = SpringContextUtils.getApplicationContext().getBeanNamesForType(DataSource.class);
             AssertUtils.notEmpty(beanNames, "未获取到数据源");
             if (beanNames.length == 1) {
+                log.info("获取到数据源为：{}", SpringContextUtils.getBean(beanNames[0]).toString());
                 this.jdbcTemplate = new JdbcTemplate(SpringContextUtils.getBean(beanNames[0]));
             } else {
-                AssertUtils.notBlank(dataSourceName, "数据源名称不能为空");
+                AssertUtils.notBlank(dataSourceName, "存在多数据源，数据源名称不能为空");
                 AssertUtils.isTrue(Arrays.asList(beanNames).contains(dataSourceName), "未匹配到数据源");
+                log.info("获取到数据源为：{}", SpringContextUtils.getBean(beanNames[0]).toString());
                 this.jdbcTemplate = new JdbcTemplate(SpringContextUtils.getBean(dataSourceName));
             }
         }
