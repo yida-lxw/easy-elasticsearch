eabe38f6f53c0aa8b6a0b773580f786669d2e83e
liangbaole <liangbaole@yzw.cn>
2024-10-31 16:39:57 +0800

    数据迁移

--- jc2-common-data-transfer/src/main/java/cn/yzw/jc2/common/transfer/utils/CommonUtils.java
+++ jc2-common-data-transfer/src/main/java/cn/yzw/jc2/common/transfer/utils/CommonUtils.java
@@ -14,12 +14,12 @@ public class CommonUtils {
         String[] beanNames = SpringContextUtils.getApplicationContext().getBeanNamesForType(DataSource.class);
         AssertUtils.notEmpty(beanNames, "未获取到数据源");
         if (beanNames.length == 1) {
-            log.info("获取到数据源为：{}", SpringContextUtils.getBean(beanNames[0]).toString());
+            log.info("获取到数据源为：{}", beanNames[0]);
             return new JdbcTemplate(SpringContextUtils.getBean(beanNames[0]));
         } else {
             AssertUtils.notBlank(dataSourceName, "存在多数据源，数据源名称不能为空");
             AssertUtils.isTrue(Arrays.asList(beanNames).contains(dataSourceName), "未匹配到数据源");
-            log.info("获取到数据源为：{}", SpringContextUtils.getBean(beanNames[0]).toString());
+            log.info("获取到数据源为：{}", dataSourceName);
             return new JdbcTemplate(SpringContextUtils.getBean(dataSourceName));
         }
 
